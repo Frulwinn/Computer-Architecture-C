db2077faf05c394048de5b64056c0b9abc44f9cd
Frulwinn <frulwinn@gmail.com>
2019-07-29 17:58:34 -0500

    fixed struct cpu

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -4,18 +4,15 @@
 // Holds all information about the CPU
 struct cpu {
   // TODO
-  //The type for a single unsigned byte in C is:
-  unsigned char x;
-
   // PC address of currently executing instruction is held in a special register called program counter
   //index of current instruction
-  int pc = 0
+  int PC;
 
   // registers (array) //we'll execute code that stores the value 8 in a register
   unsigned char registers[8];
 
   // ram (array) //8-bit memory addressing
-  unsigned char memory[8];
+  unsigned char memory[256];
 }
 
 // ALU operations
